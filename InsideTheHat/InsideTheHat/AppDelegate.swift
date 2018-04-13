40dcc6248197b1a3351e5be855d2bfd3720c15fb
Sourav Chatterjee <sourav.chatterjee@indianic.com>
2018-04-13 12:07:35 +0530

    - Initial upload

---
+++ InsideTheHat/InsideTheHat/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  InsideTheHat
+//
+//  Created by indianic on 13/04/18.
+//  Copyright © 2018 Sourav. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
